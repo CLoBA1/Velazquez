3228c18c42ebbb5bb1a00c9daf8a9c53bc359497
CLoBA <paco79455@gmail.com>
2026-02-11 12:30:55 -0600

    feat: update icons for Ferreteria and Maquinaria

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -44,7 +44,7 @@
                         class="w-16 h-16 bg-gray-100 rounded-2xl flex items-center justify-center mb-6 text-primary group-hover:bg-primary group-hover:text-secondary transition-colors duration-300">
                         <svg class="w-8 h-8" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                d="M20 7l-8-4-8 4m16 0l-8 4m8-4v10l-8 4m0-10L4 7m8 4v10M4 7v10l8 4" />
+                                d="M14.7 6.3a1 1 0 0 0 0 1.4l1.6 1.6a1 1 0 0 0 1.4 0l3.7-3.7a1 1 0 0 0 0-1.4 1 1 0 0 0-1.4 0L14.7 6.3zm-3.2 1.6a1 1 0 0 0-1.4 0L2.3 15.7a1 1 0 0 0 0 1.4l2.6 2.6a1 1 0 0 0 1.4 0l7.8-7.8a1 1 0 0 0 0-1.4l-2.6-2.6z" />
                         </svg>
                     </div>
 
@@ -100,7 +100,8 @@
                         class="absolute top-0 right-0 p-6 opacity-5 group-hover:opacity-10 transition-opacity transform group-hover:scale-110 duration-500">
                         <svg class="w-32 h-32 text-yellow-600" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5"
-                                d="M13 10V3L4 14h7v7l9-11h-7z" />
+                                d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z" />
+                            <path d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
                         </svg>
                     </div>
 
@@ -108,7 +109,7 @@
                         class="w-16 h-16 bg-yellow-50 rounded-2xl flex items-center justify-center mb-6 text-yellow-600 group-hover:bg-yellow-500 group-hover:text-white transition-colors duration-300">
                         <svg class="w-8 h-8" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                d="M19.428 15.428a2 2 0 00-1.022-.547l-2.384-.477a6 6 0 00-3.86.517l-.318.158a6 6 0 01-3.86.517L6.05 15.21a2 2 0 00-1.806.547M8 4h8l-1 1v5.172a2 2 0 00.586 1.414l5 5c1.26 1.26.367 3.414-1.415 3.414H4.828c-1.782 0-2.674-2.154-1.414-3.414l5-5A2 2 0 009 10.172V5L8 4z" />
+                                d="M19.8 17.6c.4.5.2 1.3-.4 1.5-.2.1-.5.1-.7.1-1.3 0-2.5-.5-3.4-1.4l-3-3C11.5 14.1 10.3 13.9 9 14.1l-.2.1c-1.3.1-2.6.4-3.7 1-.7.4-1.6.4-2.2-.2L2 14c-.3-.3-.4-.8-.1-1.2l2.3-3.6c.3-.5.9-.7 1.5-.7h3.8c.3 0 .6.1.9.2l1.3.6c.7.3 1.5.3 2.2 0l4.3-2c.6-.3 1.3-.2 1.8.3l1.5 1.5c.5.5.5 1.3 0 1.8l-1.9 1.9c-.3.3-.3.8 0 1.1l1.5 1.5c.3.3.3.8 0 1.2z" />
                         </svg>
                     </div>
 
